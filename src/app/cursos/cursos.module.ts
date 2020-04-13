18ea1989cf2ebe433461da9034c1010d1ebaf534
vini-politta <vinicius.politta3@gmail.com>
2020-04-13 17:20:01 -0300

    aula 58

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,20 +1,26 @@
+import { CursosService } from './cursos.service';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CursosRoutingModule } from './cursos-routing.module';
+import { RouterModule } from '@angular/router';
+
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursosComponent } from './cursos.component';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-import { CursosRoutingModule } from './cursos-routing.module';
 
 
 @NgModule({
-  declarations: [CursosComponent, CursoDetalheComponent, CursoNaoEncontradoComponent],
+  declarations: [
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent],
   imports: [
     CommonModule,
     CursosRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule
   ],
-  providers: []
+  providers: [CursosService]
 })
 export class CursosModule { }
